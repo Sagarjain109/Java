052913490eb67dfbffc624b7bc461b82722ffc64
Sagarjain109 <92741997+Sagarjain109@users.noreply.github.com>
2022-10-18 21:52:49 -0700

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,10 @@
+package Sagar;
+
+public class Main1 {
+    public static void main(String [] args){
+        Prod p = new Prod();
+        p.add();
+        p.mul();
+
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,24 @@
+package Sagar;
+
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Sum {
+    void add(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first value :");
+        int a = sc.nextInt();
+        System.out.println("Enter first value :");
+        int b = sc.nextInt();
+        int res = a+b;
+        System.out.println("Your sum is :"+res);
+
+    }
+    public static void main(String [] args){
+
+
+    }
+}
